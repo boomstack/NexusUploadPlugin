6872faecfae4c1c4677efa14e6b20799f06d2c26
wangkangfei <wangkangfei@baijiahulian.com>
2016-10-25 16:17:30 +0800

    upload success

---
+++ nexusupload/build/tmp/groovydoc/com/boomstack/nexusupload/ExtraArchivePluginExtension.groovy
@@ -0,0 +1,7 @@
+package com.boomstack.nexusupload
+
+class ExtraArchivePluginExtension {
+    Boolean sources = Boolean.TRUE
+    Boolean tests = Boolean.FALSE
+    Boolean javadoc = Boolean.TRUE
+}
